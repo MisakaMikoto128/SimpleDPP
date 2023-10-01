f2b9990b67c25e08e9b4f2910e02d34de04d37bb
Misakamikoto128 <liuyuanlins@outlook.com>
2023-10-01 14:08:08 +0800

    增加CRC分段计算方法

---
+++ lib/crc.h
@@ -0,0 +1,18 @@
+/**
+ * @file crc.h
+ * @author Liu Yuanlin (liuyuanlins@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-08
+ * 
+ * @copyright Copyright (c) 2022 Liu Yuanlin Personal.
+ * 
+ */
+#ifndef CRC_H
+#define CRC_H
+#include <stdint.h>
+uint16_t CRC16_Modbus(const uint8_t *_pBuf, uint16_t _usLen);
+uint16_t CRC16_Modbus_start();
+uint16_t CRC16_Modbus_update(uint16_t crc, const uint8_t *_pBuf, uint16_t _usLen);
+
+#endif // !CRC_H
